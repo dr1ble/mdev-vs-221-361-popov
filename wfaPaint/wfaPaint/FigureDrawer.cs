0130acae58f559690672216542e8c9a486ad4960
Nikita Popov <nikita.popovv@list.ru>
2025-04-18 15:59:49 +0300

    labs

---
+++ wfaPaint/wfaPaint/FigureDrawer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfaPaint
+{
+    public static class FigureDrawer
+    {
+        public static void DrawLine(Graphics g, Pen pen, Point start, Point end)
+        {
+            g.DrawLine(pen, start, end);
+        }
+
+        public static void DrawRectangle(Graphics g, Pen pen, Point start, Point end)
+        {
+            var rect = GetRect(start, end);
+            g.DrawRectangle(pen, rect);
+        }
+
+        public static void DrawEllipse(Graphics g, Pen pen, Point start, Point end)
+        {
+            var rect = GetRect(start, end);
+            g.DrawEllipse(pen, rect);
+        }
+
+        public static void DrawTriangle(Graphics g, Pen pen, Point start, Point end)
+        {
+            int w = end.X - start.X;
+            int h = end.Y - start.Y;
+            Point p1 = new(start.X + w / 2, start.Y);
+            Point p2 = new(start.X + w, start.Y + h);
+            Point p3 = new(start.X, start.Y + h);
+            g.DrawPolygon(pen, new[] { p1, p2, p3 });
+        }
+
+        private static Rectangle GetRect(Point start, Point end)
+        {
+            return new Rectangle(
+                Math.Min(start.X, end.X),
+                Math.Min(start.Y, end.Y),
+                Math.Abs(end.X - start.X),
+                Math.Abs(end.Y - start.Y));
+        }
+
+        public static void DrawArrow(Graphics g, Pen pen, Point start, Point end)
+        {
+            using AdjustableArrowCap arrowCap = new(6, 6);
+            Pen arrowPen = (Pen)pen.Clone();
+            arrowPen.CustomEndCap = arrowCap;
+            g.DrawLine(arrowPen, start, end);
+            arrowPen.Dispose();
+        }
+
+        public static void DrawStar(Graphics g, Pen pen, Point start, Point end)
+        {
+            var rect = GetRect(start, end);
+            int cx = rect.X + rect.Width / 2;
+            int cy = rect.Y + rect.Height / 2;
+            int outerR = Math.Min(rect.Width, rect.Height) / 2;
+            int innerR = outerR / 2;
+
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                double angle = Math.PI / 5 * i - Math.PI / 2;
+                int r = i % 2 == 0 ? outerR : innerR;
+                points[i] = new Point(
+                    (int)(cx + r * Math.Cos(angle)),
+                    (int)(cy + r * Math.Sin(angle)));
+            }
+            g.DrawPolygon(pen, points);
+        }
+
+        public static void DrawHexagon(Graphics g, Pen pen, Point start, Point end)
+        {
+            var rect = GetRect(start, end);
+            int cx = rect.X + rect.Width / 2;   
+            int cy = rect.Y + rect.Height / 2;
+            int r = Math.Min(rect.Width, rect.Height) / 2;
+
+            Point[] points = new Point[6];
+            for (int i = 0; i < 6; i++)
+            {
+                double angle = Math.PI / 3 * i - Math.PI / 2;
+                points[i] = new Point(
+                    (int)(cx + r * Math.Cos(angle)),
+                    (int)(cy + r * Math.Sin(angle)));
+            }
+            g.DrawPolygon(pen, points);
+        }
+    }
+
+}
---
+++ wfaPaint/wfaPaint/wfaPaint.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace wfaPaint
+{
+    public partial class wfaPaint : Form
+    {
+        private enum MyDrawMode
+        {
+            Pencil,
+            Line,
+            Ellipse,
+            Rectangle,
+            Triangle,
+            Arrow,
+            Star,
+            Hexagon,
+            Select
+        }
+
+        private DrawingManager drawingManager;
+        private Point startLocation;
+        private MyDrawMode drawMode = MyDrawMode.Pencil;
+        private bool isDrawing = false;
+        private Point dragOffset; // смещение курсора относительно угла выделенной области
+
+
+        private Rectangle? selectedArea = null;
+        private Bitmap? selectedBitmap = null;
+        private bool isDragging = false;
+
+        public wfaPaint()
+        {
+            InitializeComponent();
+
+            drawingManager = new DrawingManager(Screen.PrimaryScreen.Bounds.Width,
+                                                Screen.PrimaryScreen.Bounds.Height,
+                                                panel2.BackColor, 10);
+
+            panel2.Click += (s, e) => drawingManager.ChangeColor(panel2.BackColor);
+            panel3.Click += (s, e) => drawingManager.ChangeColor(panel3.BackColor);
+            panel4.Click += (s, e) => drawingManager.ChangeColor(panel4.BackColor);
+            panel5.Click += (s, e) => drawingManager.ChangeColor(panel5.BackColor);
+
+            buSelectPen.Click += (s, e) => drawMode = MyDrawMode.Pencil;
+            buSelectLine.Click += (s, e) => drawMode = MyDrawMode.Line;
+            buSelectEllipse.Click += (s, e) => drawMode = MyDrawMode.Ellipse;
+            buSelectRectangle.Click += (s, e) => drawMode = MyDrawMode.Rectangle;
+            buSelectTriangle.Click += (s, e) => drawMode = MyDrawMode.Triangle;
+            buSelectArrow.Click += (s, e) => drawMode = MyDrawMode.Arrow;
+            buSelectStar.Click += (s, e) => drawMode = MyDrawMode.Star;
+            buSelectHexagon.Click += (s, e) => drawMode = MyDrawMode.Hexagon;
+            buModeSelect.Click += (s, e) => drawMode = MyDrawMode.Select;
+
+            buSaveAsToFile.Click += BuSaveAsToFile_Click;
+            buLoadFromFile.Click += BuLoadFromFile_Click;
+            buNewImage.Click += BuNewImage_Click;
+
+            trPenWidth.Minimum = 1;
+            trPenWidth.Maximum = 12;
+            trPenWidth.Value = (int)drawingManager.Pen.Width;
+            trPenWidth.ValueChanged += (s, e) => drawingManager.ChangePenWidth(trPenWidth.Value);
+
+            pxImage.MouseDown += PxImage_MouseDown;
+            pxImage.MouseMove += PxImage_MouseMove;
+            pxImage.MouseUp += PxImage_MouseUp;
+            pxImage.Paint += (s, e) =>
+            {
+                e.Graphics.DrawImage(drawingManager.Bitmap, 0, 0);
+                if (drawMode == MyDrawMode.Select && selectedArea.HasValue && !isDragging)
+                {
+                    using Pen dashed = new Pen(Color.Black) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash };
+                    e.Graphics.DrawRectangle(dashed, selectedArea.Value);
+                }
+            };
+        }
+
+        private void BuNewImage_Click(object? sender, EventArgs e)
+        {
+            drawingManager = new DrawingManager(Screen.PrimaryScreen.Bounds.Width,
+                                                Screen.PrimaryScreen.Bounds.Height,
+                                                drawingManager.Pen.Color,
+                                                (int)drawingManager.Pen.Width);
+            trPenWidth.Value = (int)drawingManager.Pen.Width;
+            pxImage.Invalidate();
+        }
+
+        private void BuSaveAsToFile_Click(object? sender, EventArgs e)
+        {
+            using SaveFileDialog dialog = new();
+            dialog.Filter = "PNG Files(*.PNG)|*.PNG";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingManager.Bitmap.Save(dialog.FileName);
+            }
+        }
+
+        private void BuLoadFromFile_Click(object? sender, EventArgs e)
+        {
+            using OpenFileDialog dialog = new();
+            dialog.Filter = "PNG Files(*.PNG)|*.PNG";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingManager.LoadImage(dialog.FileName);
+                pxImage.Invalidate();
+            }
+        }
+
+        private void PxImage_MouseDown(object? sender, MouseEventArgs e)
+        {
+            startLocation = e.Location;
+            isDrawing = true;
+
+            if (drawMode == MyDrawMode.Select && selectedArea.HasValue && selectedArea.Value.Contains(e.Location))
+            {
+                isDragging = true;
+
+                var oldRect = selectedArea.Value;
+
+                dragOffset = new Point(e.X - oldRect.X, e.Y - oldRect.Y); // ⬅️ фиксируем смещение
+
+                selectedBitmap = new Bitmap(oldRect.Width, oldRect.Height);
+                using (Graphics g = Graphics.FromImage(selectedBitmap))
+                {
+                    g.DrawImage(drawingManager.Bitmap, new Rectangle(0, 0, oldRect.Width, oldRect.Height), oldRect, GraphicsUnit.Pixel);
+                }
+
+                drawingManager.BackupImage();
+
+                using (Graphics g = Graphics.FromImage(drawingManager.Bitmap))
+                {
+                    Bitmap transparent = new Bitmap(oldRect.Width, oldRect.Height, PixelFormat.Format32bppArgb);
+                    g.DrawImage(transparent, oldRect);
+                }
+            }
+            else
+            {
+                drawingManager.BackupImage();
+            }
+        }
+
+
+        private void PxImage_MouseMove(object? sender, MouseEventArgs e)
+        {
+            if (!isDrawing || e.Button != MouseButtons.Left) return;
+
+            if (drawMode == MyDrawMode.Select && isDragging && selectedArea.HasValue && selectedBitmap != null)
+            {
+                var newTopLeft = new Point(e.X - dragOffset.X, e.Y - dragOffset.Y);
+                var oldRect = selectedArea.Value;
+                var newRect = new Rectangle(newTopLeft, oldRect.Size);
+
+                drawingManager.RestoreBackup();
+                drawingManager.Graphics.DrawImage(selectedBitmap, newRect);
+
+                selectedArea = newRect;
+                pxImage.Invalidate();
+                return;
+            }
+            else if (drawMode == MyDrawMode.Select)
+            {
+                var rect = GetRect(startLocation, e.Location);
+                selectedArea = rect;
+
+                selectedBitmap?.Dispose();
+                selectedBitmap = null;
+
+                if (rect.Width > 0 && rect.Height > 0)
+                {
+                    selectedBitmap = drawingManager.Bitmap.Clone(rect, drawingManager.Bitmap.PixelFormat);
+                }
+
+                pxImage.Invalidate();
+            }
+
+            else
+            {
+                drawingManager.RestoreBackup();
+
+                switch (drawMode)
+                {
+                    case MyDrawMode.Pencil:
+                        FigureDrawer.DrawLine(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        startLocation = e.Location;
+                        break;
+                    case MyDrawMode.Line:
+                        FigureDrawer.DrawLine(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Ellipse:
+                        FigureDrawer.DrawEllipse(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Rectangle:
+                        FigureDrawer.DrawRectangle(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Triangle:
+                        FigureDrawer.DrawTriangle(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Arrow:
+                        FigureDrawer.DrawArrow(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Star:
+                        FigureDrawer.DrawStar(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                    case MyDrawMode.Hexagon:
+                        FigureDrawer.DrawHexagon(drawingManager.Graphics, drawingManager.Pen, startLocation, e.Location);
+                        break;
+                }
+
+                pxImage.Invalidate();
+            }
+        }
+
+
+        private void PxImage_MouseUp(object? sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+
+            if (isDragging)
+            {
+                isDragging = false;
+
+                selectedArea = null;
+                selectedBitmap?.Dispose();
+                selectedBitmap = null;
+
+                pxImage.Invalidate();
+            }
+        }
+
+        private Rectangle GetRect(Point a, Point b)
+        {
+            return new Rectangle(
+                Math.Min(a.X, b.X),
+                Math.Min(a.Y, b.Y),
+                Math.Abs(a.X - b.X),
+                Math.Abs(a.Y - b.Y));
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.V))
+            {
+                PasteFromClipboard();
+                return true;
+            }
+
+            if (selectedArea.HasValue)
+            {
+                if (keyData == (Keys.Control | Keys.C))
+                {
+                    CopySelectionToClipboard();
+                    return true;
+                }
+
+                if (keyData == (Keys.Control | Keys.X))
+                {
+                    CopySelectionToClipboard();
+                    DeleteSelection();
+                    return true;
+                }
+
+                if (keyData == Keys.Delete)
+                {
+                    DeleteSelection();
+                    return true;
+                }
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+        private void CopySelectionToClipboard()
+        {
+            if (selectedBitmap != null)
+            {
+                Clipboard.SetImage(selectedBitmap);
+            }
+        }
+
+        private void PasteFromClipboard()
+        {
+            try
+            {
+                IDataObject data = Clipboard.GetDataObject();
+                if (data != null && data.GetDataPresent(DataFormats.Bitmap))
+                {
+                    var bitmap = (Bitmap)data.GetData(DataFormats.Bitmap);
+                    if (bitmap == null) return;
+
+                    drawingManager.BackupImage();
+
+                    // Вставить в центр холста
+                    int x = (drawingManager.Bitmap.Width - bitmap.Width) / 2;
+                    int y = (drawingManager.Bitmap.Height - bitmap.Height) / 2;
+
+                    drawingManager.Graphics.DrawImage(bitmap, x, y);
+                    pxImage.Invalidate();
+                }
+                else
+                {
+                    MessageBox.Show("Буфер обмена не содержит изображение", "Вставка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка вставки изображения: " + ex.Message);
+            }
+        }
+
+
+
+        private void DeleteSelection()
+        {
+            if (selectedArea.HasValue)
+            {
+                Rectangle rect = selectedArea.Value;
+                using SolidBrush transparent = new(Color.FromArgb(0, 255, 255, 255));
+                drawingManager.Graphics.FillRectangle(transparent, rect);
+
+                selectedArea = null;
+                selectedBitmap?.Dispose();
+                selectedBitmap = null;
+                pxImage.Invalidate();
+            }
+        }
+
+    }
+}
